3a6493776c9a4c171946155a7cd97de9376878e2
Yash Raj <ryash072007@gmail.com>
2025-06-21 20:55:10 +0400

    Add video frame extraction and processing functionality
    - Implemented frame_to_base64 function to convert video frames to base64 strings.
    - Added extract_every_x_frames function to extract frames from a video at specified intervals.
    - Integrated LLMChain for processing extracted frames with a detailed prompt for analysis.
    - Updated .gitignore to exclude temporary files.

---
+++ LLMChain.py
@@ -0,0 +1,258 @@
+from groq import Groq
+import logging
+import types
+import json
+
+logger = logging.getLogger(__name__)
+
+
+class BaseLLMLink:
+    def __init__(self):
+        pass
+
+
+class ImageGroqLink(BaseLLMLink):
+    def __init__(
+        self,
+        _client: Groq,
+        _system_prompt: str,
+        _model: str = "meta-llama/llama-4-scout-17b-16e-instruct",
+    ):
+        self.client: Groq = _client
+        self.model: str = _model
+        self.system_prompt = _system_prompt
+
+    def forward(self, _image_b64: str) -> str:
+        if _image_b64 is None:
+            return None
+
+        message_to_send: dict = {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": self.system_prompt},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{_image_b64}",
+                    },
+                },
+            ],
+        }
+
+        chat_completion = self.client.chat.completions.create(
+            messages=[message_to_send],
+            model=self.model
+        )
+
+        return chat_completion.choices[0].message.content
+        
+
+class AudioGroqLink(BaseLLMLink):
+    def __init__(
+        self,
+        _client: Groq,
+        _model: str,
+    ):
+        self.client: Groq = _client
+        self.model: str = _model
+
+    def forward(self, _wav_file: str) -> str:
+
+        if _wav_file is None:
+            return None
+
+        with open(_wav_file, "rb") as file:
+            transcription = self.client.audio.transcriptions.create(
+                file=file,
+                model=self.model,
+                language="en",
+                temperature=0.3
+            )
+            return transcription.text
+
+
+class LLMChain(BaseLLMLink):
+    def __init__(self, _name: str, _llm_links: list):
+        self.llm_links: list = _llm_links
+        self.name = _name
+        logger.debug(f"LLM classes provided: {self.llm_links}")
+
+        super().__init__()
+
+    def forward(self, prompt):
+        logger.info(f"Starting generation for LLM Chain: {self.name}")
+        self.next_layer_input = prompt
+        for idx, link in enumerate(self.llm_links):
+            logger.info(f"Processing link at idx: {idx}")
+            if isinstance(link, BaseLLMLink):
+                self.next_layer_input = link.forward(self.next_layer_input)
+            elif isinstance(link, types.FunctionType):
+                self.next_layer_input = link(self.next_layer_input)
+            else:
+                logger.error(f"Undefined type of link found in LLMChain: {type(link)}")
+        return self.next_layer_input
+
+
+class JSON2StrLink(BaseLLMLink):
+    def __init__(self, _key: str):
+        self.key = _key
+        super().__init__()
+
+    def forward(self, _prompt: dict):
+        logger.info(f"JSON Prompt: {_prompt}")
+        return str(_prompt[self.key])
+
+
+class SplitterLink(BaseLLMLink):
+    def __init__(self, _splits: dict):
+        self.splits = _splits
+        super().__init__()
+
+    def forward(self, _prompts: dict) -> dict:
+        return_prompt = {}
+        if len(self.splits) != len(_prompts):
+            logger.warning(
+                f"Split size of {len(self.splits)} does not match that of prompts: {len(_prompts)}"
+            )
+        for key, link in self.splits.items():
+            prompt = _prompts[key]
+            output = None
+            if isinstance(link, BaseLLMLink):
+                output = link.forward(prompt)
+            elif isinstance(link, types.FunctionType):
+                output = link(prompt)
+            else:
+                logger.error(
+                    f"Undefined type of link found in SplitterLink: {type(link)}"
+                )
+            return_prompt[key] = output
+        return return_prompt
+
+
+class TextGroqLink(BaseLLMLink):
+    def __init__(
+        self,
+        _client: Groq,
+        _model: str,
+        _system_prompt: str = None,
+        _use_memory: bool = True,
+        _memory_size: int = None,
+    ):
+        self.client: Groq = _client
+        self.model: str = _model
+        self.system_prompt: str = _system_prompt
+
+        logger.info(f"TextGroqLink System prompt: {_system_prompt}")
+
+        self.use_memory: bool = _use_memory
+        self.memory_size: int = _memory_size
+
+        self.memory: list = []
+        if self.system_prompt:
+            self.memory.append({"role": "system", "content": self.system_prompt})
+
+        if not self.use_memory and _memory_size is not None:
+            logger.warning(
+                "You should not specify memory_size when use_memory is False."
+            )
+        elif self.use_memory and _memory_size is None:
+            logger.warning(
+                "Using memory but memory_size is not specified. Memory will increase and indefinetely and could hit rate limits fast."
+            )
+
+    def forward(self, _prompt: str) -> str:
+
+        if _prompt is None:
+            return ""
+
+        message_to_send: dict = {
+            "role": "user",
+            "content": _prompt,
+        }
+
+        logger.info(f"TextGroqLink PROMPT: {_prompt}")
+
+        send_messages: list = self.memory + [message_to_send]
+
+        chat_completion = self.client.chat.completions.create(
+            messages=send_messages,
+            model=self.model,
+            stream=False,
+        )
+
+        response_message = chat_completion.choices[0].message
+
+        if self.use_memory:
+            self.memory.extend([message_to_send, response_message])
+            if self.memory_size is not None:
+                size_diff: int = len(self.memory) - self.memory_size
+                if size_diff > 0:
+                    del self.memory[1 : 1 + size_diff]
+
+        return response_message.content
+
+
+class JSONGroqLink(TextGroqLink):
+
+    # Overiding the forward function of JSONGroqLink
+    def forward(self, _prompt: str) -> dict:
+
+        message_to_send: dict = {
+            "role": "user",
+            "content": _prompt,
+        }
+
+        logger.info(f"JSONGroqLink PROMPT: {_prompt}")
+
+        send_messages: list = self.memory + [message_to_send]
+
+        chat_completion = self.client.chat.completions.create(
+            messages=send_messages,
+            model=self.model,
+            stream=False,
+            response_format={"type": "json_object"},
+        )
+
+        response_message = chat_completion.choices[0].message
+
+        if self.use_memory:
+            self.memory.extend([message_to_send, response_message])
+            if self.memory_size is not None:
+                size_diff: int = len(self.memory) - self.memory_size
+                if size_diff > 0:
+                    del self.memory[1 : 1 + size_diff]
+
+        return json.loads(response_message.content)
+
+
+if __name__ == "__main__":
+    import os
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    logging.basicConfig(level=logging.INFO)
+
+    client = Groq(
+        api_key=os.environ.get("GROQ_API_KEY"),
+    )
+
+    llm_chain = LLMChain(
+        "main",
+        [
+            TextGroqLink(
+                client, "gemma2-9b-it", "You are a question generation agent", True, 3
+            ),
+            TextGroqLink(
+                client,
+                "llama3-70b-8192",
+                "You are an question answering agent that answers with only 10 tokens",
+                False,
+            ),
+        ],
+    )
+
+    result = llm_chain.forward("Generate a question about UAE")
+    print(result)
+
+    result = llm_chain.forward("Generate the same question again")
+    print(result)
---
+++ extractor.py
@@ -0,0 +1,69 @@
+from moviepy import VideoClip, VideoFileClip
+import numpy as np
+from PIL import Image
+from LLMChain import *
+import base64
+import os
+from dotenv import load_dotenv
+import io
+
+debug = True
+
+def frame_to_base64(frame: np.ndarray) -> str:
+    """
+    Convert a numpy array frame to base64 encoded string.
+    
+    Args:
+        frame: Numpy array representing an image frame
+        
+    Returns:
+        Base64 encoded string of the image
+    """
+    image = Image.fromarray(frame)
+    
+    buffer = io.BytesIO()
+    
+    image.save(buffer, format="JPEG")
+    
+    img_bytes = buffer.getvalue()
+    img_base64 = base64.b64encode(img_bytes).decode("utf-8")
+    
+    return img_base64
+
+def extract_every_x_frames(x: int, video: VideoClip) -> list:
+    frames = []
+    i = 0
+    for frame in video.iter_frames():
+        i += 1
+        if i % x != 0:
+            continue
+        frames.append(frame)
+        if debug:
+            save_image = Image.fromarray(frame)
+            save_image.save(f"temp/image_{i*x}.jpg")
+    return frames
+
+video_path = "video.mp4"
+clip = VideoFileClip(video_path)
+
+frames = extract_every_x_frames(15, clip)
+
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+sys_prompt = open("image_sys_prompt.txt").read()
+
+bot = LLMChain(
+    "sunfish-v0",
+    [
+        ImageGroqLink(client, sys_prompt)
+    ]
+)
+
+for frame in frames[:1]:
+    result = bot.forward(frame_to_base64(frame))
+    print(result)
