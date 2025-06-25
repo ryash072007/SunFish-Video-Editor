9b7334b4f51fc9898acf73177f3f815ab201fa15
Gopesh Janardhanan <68292507+Gopesh456@users.noreply.github.com>
2025-06-25 22:23:21 +0400

    Revert "Merge branch 'main' of https://github.com/ryash072007/SunFish-Video-Editor"
    This reverts commit 72f2c6c81dd1cbbd4125167bd6ed0f13abd9e871, reversing
    changes made to c074fbcb05a713311e17cbe23158f17ccd70a3a4.

--- LLMChain.py
+++ LLMChain.py
@@ -71,11 +71,20 @@ class ImageGroqLink(BaseLLMLink):
 
         send_messages: list = self.memory + [message_to_send]
 
-        chat_completion = self.client.chat.completions.create(
-            messages=send_messages,
-            model=self.model,
-            stream=False
-        )
+        try:
+            chat_completion = self.client.chat.completions.create(
+                messages=send_messages,
+                model=self.model,
+                stream=False
+            )
+        except APIStatusError as e:
+            if e.status_code == 429:
+                reset_req = e.response.headers.get("x-ratelimit-reset-requests")
+                wait_sec = parse_duration(reset_req)
+                print(f"Sleepin for {wait_sec}s")
+                time.sleep(wait_sec)
+                self.forward(_image_b64)
+
 
         response_message = chat_completion.choices[0].message
 
